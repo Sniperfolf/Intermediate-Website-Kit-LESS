1d05f6a3de0f6300eb5a1f2f1ab3a6d8ab56de87
Sniperfolf <136292695+Sniperfolf@users.noreply.github.com>
2024-07-30 20:39:01 -0600

    Updated contact info

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "name": "Code Stitch Web Designs",
-    "email": "help@codestitch.app",
-    "phoneForTel": "555-779-4407",
-    "phoneFormatted": "(555) 779-4407",
+    "name": "Seras V Web Designs",
+    "email": "Admin@SerasVDesigns.com",
+    "phoneForTel": "575-910-3443",
+    "phoneFormatted": "(575) 910-3443",
     "address": {
-        "lineOne": "First Address Line",
-        "lineTwo": "Second Address Line",
-        "city": "Denver",
-        "state": "CO",
-        "zip": "80206",
+        "lineOne": "5203 Juan Tabo Blvd. NE",
+        "lineTwo": "Suite 2B",
+        "city": "Albuquerque ",
+        "state": "NM",
+        "zip": "87111",
         "mapLink": "https://goo.gl/maps/UAQn4vuGDiwv7DV39"
     },
     "socials": {
@@ -16,5 +16,5 @@ module.exports = {
         "instagram": "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    "domain": "https://www.example.com"
+    "domain": "https://www.SerasVDesigns.com"
 };
